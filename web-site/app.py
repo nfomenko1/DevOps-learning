b47616cc9d8a5003e4e7d0e7b258d59e6a3355ec
nfomenko1@mail.ru <nfomenko1@mail.ru>
2025-12-29 13:14:50 +0300

    Upadate + new folders web-site + practice web-site

--- web-site/app.py
+++ web-site/app.py
@@ -1,1 +1,10 @@
-1111
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Привет, это RoadMap-DevOps Блок 1!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
